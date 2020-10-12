8e79e187a75b397caab538e01ca0ca77cd2496cf
Antoine Kurka <44458979+AKurka@users.noreply.github.com>
2020-10-12 11:38:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ MessageCrypte="oh kdfkdjh yrxv shuphwwudlw gdvvxuhu od frqilghqwldolwh vlpsohphq
 lg=len(MessageCrypte)
 MessageClair=""
 alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-cle=3 # Décalage par rapport à Y (code ASCII : 24 + 1 = 25e lettre de l'alphabet)
+cle=3
 
 for i in range(lg):
     if MessageCrypte[i]==' ':
